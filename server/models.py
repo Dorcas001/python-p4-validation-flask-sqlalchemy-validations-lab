642827b27ed6cc571537c11975b21c4ae8d1a47f
karimi <karimidorcas1@gmail.com>
2024-04-13 21:25:18 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -13,6 +13,20 @@ class Author(db.Model):
 
     # Add validators 
 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author must have a name.")
+        if Author.query.filter_by(name=name).first() is not None:
+            raise ValueError("An author with this name already exists.")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if phone_number and (not phone_number.isdigit() or len(phone_number) != 10):
+            raise ValueError("Phone number must be exactly ten digits.")
+        return phone_number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -28,6 +42,32 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Content must be at least 250 characters long")
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) > 250:
+            raise ValueError("Summary should have a maximum of  250 characters ")
+        return summary
+    
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'")
+        return category
+        
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Title must contain one of the following phrases: 'Won't Believe', 'Secret', 'Top', 'Guess'")
+        return title
 
 
     def __repr__(self):
